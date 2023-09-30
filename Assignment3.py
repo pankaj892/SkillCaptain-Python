0af46c22bed1f64f9e8ad405afd0f4d540c81034
Pankaj Biradar <pankaj28988@gmail.com>
2023-09-30 20:47:02 +0530

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,17 @@
+num1 = int(input())
+num2 = int(input())
+
+addition = num1 + num2
+print(addition)
+
+subtraction = num2 - num1
+print(subtraction)
+
+multiplication = num1 * num2
+print(multiplication)
+
+if num2 == 0:
+    division = "Error: Cannot divide by zero"
+division = num1 / num2
+print(division)
+
