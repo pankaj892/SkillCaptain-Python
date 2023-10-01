726006720d05c670c1ea69394128c88d60386463
Pankaj biradar <pankaj28988@gmail.com>
2023-10-01 19:54:27 +0530

    Update as per coding conventions

--- Assignment3.py
+++ Assignment3.py
@@ -1,17 +1,19 @@
-num1 = int(input())
-num2 = int(input())
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
 
 addition = num1 + num2
-print(addition)
+print(f"Addition: {addition}")
 
 subtraction = num2 - num1
-print(subtraction)
+print(f"Subtraction: {subtraction}")
 
 multiplication = num1 * num2
-print(multiplication)
+print(f"Multiplication: {multiplication}")
 
-if num2 == 0:
-    division = "Error: Cannot divide by zero"
-division = num1 / num2
-print(division)
+try:
+    if num2 == 0:
+        print("Error: Cannot divide by zero")
+except:
+    division = num1 / num2
+    print(f"Division: {division}")
 
