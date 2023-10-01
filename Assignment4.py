feb989904cefec79c8b4c45be415d2c3a7254275
Pankaj biradar <pankaj28988@gmail.com>
2023-10-01 19:59:33 +0530

    Updates to assignment 4

--- Assignment4.py
+++ Assignment4.py
@@ -1,9 +1,9 @@
 age = int(input("Enter your age: "))
 if age < 18:
     print("You are a minor")
-if age >= 18 and age < 66:
+elif age >= 18 and age < 66:
     print("You are an adult")
-if age >= 66:
+elif age >= 66:
     print("You are a senior citizen")
 else:
     print("Error: Age cannot be negative")        
