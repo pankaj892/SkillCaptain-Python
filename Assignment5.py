7d68733040f771523ed1088167b76a3969668625
Pankaj biradar <pankaj28988@gmail.com>
2023-10-06 21:22:02 +0530

    Add assignment5

---
+++ Assignment5.py
@@ -0,0 +1,11 @@
+#Program using a while loop to print all the numbers from 1 to 10
+
+number = 1
+while number <= 10:
+    print(number)
+    number = number + 1
+print("Finished")
+
+names = ["Google","Microsoft","Facebook","Apple","IBM","Oracle","Amazon"]
+for name in names:
+    print(f'Welcome to {name}!')
