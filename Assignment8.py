855d6d623f6ca920a941c50dc3c7caecc95f1ef6
Pankaj biradar <pankaj28988@gmail.com>
2023-10-08 20:51:51 +0530

    Add assignment8.py

---
+++ Assignment8.py
@@ -0,0 +1,9 @@
+""" Python program that concatenates two lists into a single list."""
+
+list1 = [1,2,3,5] # asks the user to enter a number
+list2 = [1.7,5,1,80,9] # asks the user to enter a number
+
+def concat_lists(list1, list2):
+    return list1 + list2
+
+print(concat_lists(list1, list2)) # prints the result of the function
