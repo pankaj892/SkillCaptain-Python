ad30f2bef35c440121fb35ed19ec820303290586
Pankaj biradar <pankaj28988@gmail.com>
2023-10-08 20:25:06 +0530

    Updates to assignment7

--- Assignment7.py
+++ Assignment7.py
@@ -1,10 +1,9 @@
-# Function that takes an integer as input and returns True if the number is even, and False otherwise
+""" Function that takes an integer as input and returns True if the number is even, and False otherwise."""
 
 def is_even(number):
     if number % 2 == 0:   # if the remainder of the number divided by 2 is 0, then it is even
         return True
-    else:
-        return False      # if the remainder of the number divided by 2 is not 0, then it is odd
+    return False      # if the remainder of the number divided by 2 is not 0, then it is odd
 
 number = int(input("Enter a number: ")) # asks the user to enter a number
 print(is_even(number)) # prints the result of the function
